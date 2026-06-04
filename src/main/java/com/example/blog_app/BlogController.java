4dc33bfa7becb40c09d1ff3ee6b4c8ccfdef8a2f
Inoue <2026005@fsc-educ.com>
2026-06-04 15:34:20 +0900

    css作成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,5 +1,22 @@
 package com.example.blog_app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
   
+  // ホームの読み込み
+  @GetMapping("/")
+  public String blogs(){
+    return "blogs";
+  }
+
+  // newblogsの読み込み
+  @GetMapping("/newblogs")
+  public String blogsn(){
+    return "/newblogs";
+  }
+  
+
 }
